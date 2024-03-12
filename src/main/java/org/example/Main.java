ad4ee87c6adff9f9e19d0dc2bad3422084fb47ee
mohammad.almustafa <mohammad.almustafa@adesso.de>
2024-03-12 12:12:48 +0100

    Generic class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+class Store <T> {
+    private T item;
+    public T getItem() {
+        return item;
+    }
+    public void setItem(T item) {
+        this.item = item;
+    }
+}
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Store<String> store = new Store<>();
+        store.setItem("Hallo");
+
+        String item = store.getItem();
+        System.out.println(item);
     }
 }
